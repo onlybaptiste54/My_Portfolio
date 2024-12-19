f6b1e26bd4da2133e6f76c6c236be1926bba7bb9
OnlyBaptiste <157699823+OnlyBaptiste@users.noreply.github.com>
2024-12-19 15:41:10 +0100

    Test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Baptiste.Beroual.github.io/', // Remplace 'nom-du-repository' par le nom de ton repository GitHub
 
 })
